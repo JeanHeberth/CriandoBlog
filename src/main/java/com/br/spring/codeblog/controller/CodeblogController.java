c9a955e87d07617418fa7e1871c188df9b68b882
Jean Heberth <jeanheberth19@gmail.com>
2022-04-26 16:02:56 -0300

    Criado a mensagem de erro

--- src/main/java/com/br/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/br/spring/codeblog/controller/CodeblogController.java
@@ -4,11 +4,15 @@ import com.br.spring.codeblog.model.Post;
 import com.br.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -34,5 +38,22 @@ public class CodeblogController {
 
     }
 
+    @RequestMapping(value = "newpost", method = RequestMethod.GET)
+        public String getPostForm(){
+
+        return "postForm";
+        }
+
+    @RequestMapping(value = "newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem",
+                    "Verifique se os campos não estão vazios");
+            return  "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return  "redirect:/posts";
+    }
 
 }
